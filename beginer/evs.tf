d6b96788e31e661941e9cc72716d2bce2cf2e901
Wei HongJin <weihj1999@gmail.com>
2018-10-01 11:05:32 +0200

    update for beginner

--- beginer/evs.tf
+++
@@ -1,7 +0,0 @@
-resource "opentelekomcloud_blockstorage_volume_v2" "volume_1" { 
-    region = "${var.region_name}" 
-    name =  "${var.volume_name}" 
-    description = "first test volume" 
-    size = "${var.volume_size}"
- } 
-
