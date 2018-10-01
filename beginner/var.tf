1115e968fac3e81560a45f479a8f59a3decf9190
Wei HongJin <weihj1999@gmail.com>
2018-10-01 11:10:36 +0200

    create vpc,ecs and evs with terrafrom

---
+++ beginner/var.tf
@@ -0,0 +1,26 @@
+variable "user_name" {
+  default = "weihongjin"
+}
+variable "password" {
+  default = "Tsi8675*@"
+}
+
+variable "domain_name" {
+  default = "OTC00000000001000010501"
+}
+variable "region_name" {
+  default = "eu-de"
+}
+variable "tenant_name" {
+  default = "eu-de"
+}
+variable "endpoint" {
+  default = "https://iam.eu-de.otc.t-systems.com:443/v3"
+}
+
+variable "volume_name" {
+  default = "volume-test"
+}
+variable "volume_size" {
+  default = 10
+}
